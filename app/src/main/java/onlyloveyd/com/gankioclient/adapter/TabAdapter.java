73f0f14819d671423d6315ad42eb7db202fecb82
guagua <457420045@qq.com>
2017-03-19 20:42:28 +0800

    commit

---
+++ app/src/main/java/onlyloveyd/com/gankioclient/adapter/TabAdapter.java
@@ -0,0 +1,39 @@
+package onlyloveyd.com.gankioclient.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import onlyloveyd.com.gankioclient.fragment.GankDetailsFragment;
+
+/**
+ * Desc: {}
+ * Project Name:GankIOClient
+ * Package Name:onlyloveyd.com.gankioclient.adapter
+ * Author : Mraz
+ * Created at 2017/3/16 21:31
+ */
+
+public class TabAdapter extends FragmentPagerAdapter {
+    //福利 | Android | iOS | 休息视频 | 拓展资源 | 前端
+    public static String[] titles = {"福利","Android","iOS","休息视频","拓展资源","前端"};
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return GankDetailsFragment.newInstance(titles[position]);
+    }
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+}
