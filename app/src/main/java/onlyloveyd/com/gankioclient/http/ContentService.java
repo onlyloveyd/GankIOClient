a344ba9ce485af2e056eb5e5e3ec7773e016cbc0
onlyloveyd@gmail.com <onlyloveyd@gmail.com>
2017-03-20 17:17:12 +0800

    big modify

--- app/src/main/java/onlyloveyd/com/gankioclient/http/ContentService.java
+++ app/src/main/java/onlyloveyd/com/gankioclient/http/ContentService.java
@@ -21,4 +21,30 @@ public interface ContentService {
     Observable<HttpBean> getContent(@Path("category") String category ,
                                     @Path("pagesize") String pagesize ,
                                     @Path("pagenum") int pagenum );
+
+
+    /**
+     * 获取某天的干货
+     *
+     * @param date
+     * @return
+     */
+    @GET("day/{date}")
+    Observable<HttpBean> getRecentlyGanHuo(@Path("date") String date);
+
+    /**
+     * 搜索
+     *
+     * @param keyword
+     * @param pageIndex
+     * @return
+     */
+    @GET("search/query/{keyword}/category/{category}/count/20/page/{pageIndex}")
+    Observable<HttpBean> search(
+            @Path("category") String category
+            , @Path("keyword") String keyword
+            , @Path("pageIndex") int pageIndex);
+
+    @GET("history/content/10/{pageIndex}")
+    Observable<HttpBean> getRecently(@Path("pageIndex") int pageIndex);
 }
