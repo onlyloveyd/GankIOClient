8ec39ffb304d89ae4e5423f1d9edc5592fbee0c6
onlyloveyd@gmail.com <Yd@19911219>
2018-06-28 08:55:22 +0800

    1.修改版本，去掉无用内容

--- app/src/main/java/onlyloveyd/com/gankioclient/viewholder/BonusViewHolder.java
+++ app/src/main/java/onlyloveyd/com/gankioclient/viewholder/BonusViewHolder.java
@@ -67,124 +67,121 @@ public class BonusViewHolder extends BaseViewHolder<ResultsBean> {
                     R.mipmap.empty_data).into(ivPic);
 
             final RxPermissions rxPermissions = RxPermissionUtils.getInstance();
-            ibDownload.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (rxPermissions != null) {
-                        rxPermissions
-                                .request(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                                .subscribe(new Observer<Boolean>() {
-                                    @Override
-                                    public void onSubscribe(Disposable d) {
+            ibDownload.setOnClickListener(view -> {
+                if (rxPermissions != null) {
+                    rxPermissions
+                            .request(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            .subscribe(new Observer<Boolean>() {
+                                @Override
+                                public void onSubscribe(Disposable d) {
 
-                                    }
+                                }
 
-                                    @Override
-                                    public void onNext(Boolean granted) {
-                                        if (granted) { // Always true pre-M
-                                            // I can control the camera now
-                                            Toast.makeText(itemView.getContext(), data.getDesc() +
-                                                            Constant.SUFFIX_JPEG + "开始下载",
-                                                    Toast.LENGTH_SHORT).show();
-                                            Glide.with(itemView.getContext()).load(
-                                                    data.getUrl()).asBitmap().into(
-                                                    new SimpleTarget<Bitmap>() {
-                                                        @Override
-                                                        public void onResourceReady(
-                                                                final Bitmap resource,
-                                                                GlideAnimation<? super Bitmap>
-                                                                        glideAnimation) {
+                                @Override
+                                public void onNext(Boolean granted) {
+                                    if (granted) { // Always true pre-M
+                                        // I can control the camera now
+                                        Toast.makeText(itemView.getContext(), data.getDesc() +
+                                                        Constant.SUFFIX_JPEG + "开始下载",
+                                                Toast.LENGTH_SHORT).show();
+                                        Glide.with(itemView.getContext()).load(
+                                                data.getUrl()).asBitmap().into(
+                                                new SimpleTarget<Bitmap>() {
+                                                    @Override
+                                                    public void onResourceReady(
+                                                            final Bitmap resource,
+                                                            GlideAnimation<? super Bitmap>
+                                                                    glideAnimation) {
 
-                                                            Observer<Bitmap> subscriber =
-                                                                    new Observer<Bitmap>() {
-                                                                        @Override
-                                                                        public void onSubscribe(
-                                                                                @NonNull
-                                                                                        Disposable d) {
+                                                        Observer<Bitmap> subscriber =
+                                                                new Observer<Bitmap>() {
+                                                                    @Override
+                                                                    public void onSubscribe(
+                                                                            @NonNull
+                                                                                    Disposable d) {
 
-                                                                        }
+                                                                    }
 
-                                                                        @Override
-                                                                        public void onNext(
-                                                                                Bitmap s) {
-                                                                        }
+                                                                    @Override
+                                                                    public void onNext(
+                                                                            Bitmap s) {
+                                                                    }
 
-                                                                        @Override
-                                                                        public void onError(
-                                                                                Throwable e) {
-                                                                            Toast.makeText(
-                                                                                    itemView.getContext(),
-                                                                                    data.getDesc() +
-                                                                                            Constant.SUFFIX_JPEG
-                                                                                            +
-                                                                                            "下载失败",
-                                                                                    Toast.LENGTH_SHORT).show();
-                                                                            e.printStackTrace();
-                                                                        }
+                                                                    @Override
+                                                                    public void onError(
+                                                                            Throwable e) {
+                                                                        Toast.makeText(
+                                                                                itemView.getContext(),
+                                                                                data.getDesc() +
+                                                                                        Constant.SUFFIX_JPEG
+                                                                                        +
+                                                                                        "下载失败",
+                                                                                Toast.LENGTH_SHORT).show();
+                                                                        e.printStackTrace();
+                                                                    }
 
-                                                                        @Override
-                                                                        public void onComplete() {
-                                                                            Toast.makeText(
-                                                                                    itemView.getContext(),
-                                                                                    data.getDesc() +
-                                                                                            Constant.SUFFIX_JPEG
-                                                                                            +
-                                                                                            "下载成功",
-                                                                                    Toast.LENGTH_SHORT).show();
-                                                                        }
-                                                                    };
+                                                                    @Override
+                                                                    public void onComplete() {
+                                                                        Toast.makeText(
+                                                                                itemView.getContext(),
+                                                                                data.getDesc() +
+                                                                                        Constant.SUFFIX_JPEG
+                                                                                        +
+                                                                                        "下载成功",
+                                                                                Toast.LENGTH_SHORT).show();
+                                                                    }
+                                                                };
 
-                                                            Observable<Bitmap> observable =
-                                                                    Observable.create(
-                                                                            new ObservableOnSubscribe<Bitmap>() {
-                                                                                @Override
-                                                                                public void
-                                                                                subscribe(
-                                                                                        @NonNull
-                                                                                                ObservableEmitter<Bitmap> emitter)
-                                                                                        throws
-                                                                                        Exception {
-                                                                                    try {
-                                                                                        PublicTools.saveBitmap(
-                                                                                                resource,
-                                                                                                Environment
-                                                                                                        .getExternalStorageDirectory().getAbsolutePath()
-                                                                                                        + File.separator
-                                                                                                        + data.getDesc());
-                                                                                    } catch (Exception e) {
-                                                                                        e.printStackTrace();
-                                                                                        emitter.onError(
-                                                                                                e);
-                                                                                    }
-                                                                                    emitter.onComplete();
+                                                        Observable<Bitmap> observable =
+                                                                Observable.create(
+                                                                        new ObservableOnSubscribe<Bitmap>() {
+                                                                            @Override
+                                                                            public void
+                                                                            subscribe(
+                                                                                    @NonNull
+                                                                                            ObservableEmitter<Bitmap> emitter)
+                                                                                    throws
+                                                                                    Exception {
+                                                                                try {
+                                                                                    PublicTools.saveBitmap(
+                                                                                            resource,
+                                                                                            Environment
+                                                                                                    .getExternalStorageDirectory().getAbsolutePath()
+                                                                                                    + File.separator
+                                                                                                    + data.getDesc());
+                                                                                } catch (Exception e) {
+                                                                                    e.printStackTrace();
+                                                                                    emitter.onError(
+                                                                                            e);
                                                                                 }
-                                                                            });
+                                                                                emitter.onComplete();
+                                                                            }
+                                                                        });
 
-                                                            observable.subscribeOn(Schedulers.io())
-                                                                    .unsubscribeOn(Schedulers.io())
-                                                                    .observeOn(
-                                                                            AndroidSchedulers
-                                                                                    .mainThread())
-                                                                    .subscribe(subscriber);
-                                                        }
-                                                    });
-                                        } else {
-                                            Toast.makeText(itemView.getContext(), "请在设置中开启存储权限后再试",
-                                                    Toast.LENGTH_SHORT).show();
-                                        }
+                                                        observable.subscribeOn(Schedulers.io())
+                                                                .unsubscribeOn(Schedulers.io())
+                                                                .observeOn(
+                                                                        AndroidSchedulers
+                                                                                .mainThread())
+                                                                .subscribe(subscriber);
+                                                    }
+                                                });
+                                    } else {
+                                        Toast.makeText(itemView.getContext(), "请在设置中开启存储权限后再试",
+                                                Toast.LENGTH_SHORT).show();
                                     }
+                                }
 
-                                    @Override
-                                    public void onError(Throwable e) {
+                                @Override
+                                public void onError(Throwable e) {
 
-                                    }
+                                }
 
-                                    @Override
-                                    public void onComplete() {
+                                @Override
+                                public void onComplete() {
 
-                                    }
-                                });
-                    }
+                                }
+                            });
                 }
             });
         }
